7296d53cd51d89dee43419749107cfe51d749910
Tuomo Kinnunen <tuomo95@gmail.com>
2017-10-23 17:34:03 +0300

    -Copyright kuvien korvaaminen universaaleilla käyttökelpoisilla kuvilla. -Tekstikentän lisääminen, joka näyttää Bainin lausahdukset tekstinä samalla kun niitä toistetaan -Play -kuvakkeen lisääminen foregroundin avulla = ei tarvitse muokata suoraan kuvaan

--- app/src/main/java/tuomomees/bain_soundboard_app/MainActivity.java
+++ app/src/main/java/tuomomees/bain_soundboard_app/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +26,14 @@ public class MainActivity extends AppCompatActivity implements BainSoundPlayerTh
     int resources;
     int buttonId;
     Button button;
+    TextView bainPhraseTextView;
 
     CustomDialogClass cdd;
 
     Drawable playingOn;
     Drawable playingOff;
+
+    String bainPhraseString;
 
     int listId;
 
@@ -42,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements BainSoundPlayerTh
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bainPhraseTextView = (TextView) findViewById(R.id.bainPhraseTextView);
+        bainPhraseTextView.setSelected(true);
 
         cdd=new CustomDialogClass(this);
         playingOn = getResources().getDrawable(R.mipmap.payday2_bain_play);
@@ -65,57 +72,75 @@ public class MainActivity extends AppCompatActivity implements BainSoundPlayerTh
         switch (v.getId()){
             case R.id.buttonBain1:
                 resources = R.raw.bain_alarm;
+                bainPhraseString = "That's the alarm, time to do some heavy lifting.";
                 break;
             case R.id.buttonBain2:
                 resources = R.raw.bain_boss;
+                bainPhraseString = "Show them whos boss.";
                 break;
             case R.id.buttonBain3:
                 resources = R.raw.bain_busy;
+                bainPhraseString = "Looks like we're about to get busy.";
                 break;
             case R.id.buttonBain4:
                 resources = R.raw.bain_cleanercosts;
+                bainPhraseString = "Don't kill civilians.";
                 break;
             case R.id.buttonBain5:
                 resources = R.raw.bain_flashlight;
+                bainPhraseString = "Stay out of the flashlights people.";
                 break;
             case R.id.buttonBain6:
                 resources = R.raw.bain_go;
+                bainPhraseString = "Come on guys go!";
                 break;
             case R.id.buttonBain7:
                 resources = R.raw.bain_guys;
+                bainPhraseString = "Thats my guys.";
                 break;
             case R.id.buttonBain8:
                 resources = R.raw.bain_here_we_go;
+                bainPhraseString = "Here we go.";
                 break;
             case R.id.buttonBain9:
                 resources = R.raw.bain_hostage;
+                bainPhraseString = "Alright, let the hostage go and the trade is done.";
                 break;
             case R.id.buttonBain10:
                 resources = R.raw.bain_hostage2;
+                bainPhraseString = "a hostage.";
                 break;
             case R.id.buttonBain11:
                 resources = R.raw.bain_let_the_hostage_go;
+                bainPhraseString = "Let the hostage go and the trade is done.";
                 break;
             case R.id.buttonBain12:
                 resources = R.raw.bain_loot;
+                bainPhraseString = "Don't let them take your loot.";
                 break;
             case R.id.buttonBain13:
                 resources = R.raw.bain_sniper;
+                bainPhraseString = "Sniper rooftop.";
                 break;
             case R.id.buttonBain14:
                 resources = R.raw.bain_yes;
+                bainPhraseString = "YES.";
                 break;
             case R.id.buttonBain15:
                 resources = R.raw.bain_two;
+                bainPhraseString = "That's two one more to go.";
                 break;
             case R.id.buttonBain16:
                 resources = R.raw.bain_escape;
+                bainPhraseString = "Vans here, say it again the escape vans here.";
                 break;
             case R.id.buttonBain17:
                 resources = R.raw.bain_needcivilians;
+                bainPhraseString = "Remember guys we need ____ civilians.";
                 break;
             case R.id.buttonBain18:
                 resources = R.raw.bain_civilians;
+                bainPhraseString = "Civilians.";
                 break;
             /*
             case R.id.buttonBain19:
@@ -128,6 +153,8 @@ public class MainActivity extends AppCompatActivity implements BainSoundPlayerTh
             default:
                 resources = R.raw.bain_cleanercosts;
         }
+
+        bainPhraseTextView.setText(bainPhraseString);
 
         if(bainSoundPlayerThread == null)
         {
@@ -174,16 +201,27 @@ public class MainActivity extends AppCompatActivity implements BainSoundPlayerTh
             @Override
             public void run() {
                 boolean running = true;
+                Drawable icon;
                 while(running)
                 {
                     button = (Button) findViewById(btnId);
                         if(isButtonPlaying)
                         {
-                            button.setBackground(playingOn);
+                            //button.setBackground(playingOn);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                icon = getResources().getDrawable(R.drawable.play_icon);
+                                button.setForeground(icon);
+                            }
                         }
                         else
                         {
                             button.setBackground(playingOff);
+                            //button.setForeground();
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                button.setForeground(null);
+                                bainPhraseTextView.setText("");
+                            }
                         }
 
                     running = false;
