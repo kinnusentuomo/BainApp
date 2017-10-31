d367dc84158eb2670f873ff304c4fab92c48a7f2
tuomomees <tuomo95@gmail.com>
2017-10-31 14:25:53 +0200

    Projektin ensimmäinen pushaaminen.

---
+++ app/src/main/java/tuomomees/bain_soundboard_app/BainSoundPlayerThread.java
@@ -0,0 +1,82 @@
+package tuomomees.bain_soundboard_app;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+/**
+ * Created by tuomo on 3.10.2017.
+ */
+//
+class BainSoundPlayerThread extends Thread {
+
+    private boolean running = true;
+    private MediaPlayer mp;
+    private MediaPlayerThreadInterface observer;
+    private int sentButtonId;
+    private boolean playingAudio;
+    private int listId;
+
+    BainSoundPlayerThread(Context c, int resId, MediaPlayerThreadInterface o, int id, int id2)
+    {
+        mp = MediaPlayer.create(c, resId);
+        observer = o;
+        sentButtonId = id;
+        listId = id2;
+    }
+
+    int getSentId()
+    {
+        return sentButtonId;
+    }
+
+    int getListId()
+    {
+        return listId;
+    }
+
+    void setRunning(boolean r)
+    {
+        running = r;
+    }
+
+    boolean isPlayingAudio()
+    {
+        return playingAudio;
+    }
+
+    public void run() {
+        mp.start();
+        playingAudio = true;
+        try {
+            while (running)
+            {
+                //observer.setButtonPlaying(true, sentButtonId);
+                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        Log.d("Sound played", "stopping thread");
+                        observer.setButtonPlaying(false, sentButtonId);
+                        mp.release();
+                        playingAudio = false;
+                        observer.removeFromList(listId);
+                        running = false;
+                    }
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    interface MediaPlayerThreadInterface
+    {
+        void setButtonPlaying(boolean isButtonPlaying, int btnId);
+        void removeFromList(int id);
+    }
+}
---
+++ app/src/main/java/tuomomees/bain_soundboard_app/CustomDialogClass.java
@@ -0,0 +1,68 @@
+package tuomomees.bain_soundboard_app;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by tuomo on 10.10.2017.
+ */
+
+
+class CustomDialogClass extends Dialog implements
+        android.view.View.OnClickListener {
+
+    private Activity c;
+    public Dialog d;
+    private Button yes, no;
+
+    CustomDialogClass(Activity a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+        this.c = a;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.custom_dialog);
+        yes = (Button) findViewById(R.id.btn_yes);
+        no = (Button) findViewById(R.id.btn_no);
+        yes.setOnClickListener(this);
+        no.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_yes:
+                Toast.makeText(c.getApplicationContext(),c.getResources().getString(R.string.support_text), Toast.LENGTH_LONG).show();
+
+                String subject = c.getResources().getString(R.string.app_label_text);
+                String emailAddress = c.getResources().getString(R.string.feedback_address);
+                String templateBody = c.getResources().getString(R.string.feedback_template);
+                String whichEmailToUse =  c.getResources().getString(R.string.choose_feedback_delivering);
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto",emailAddress, null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                emailIntent.putExtra(Intent.EXTRA_TEXT, templateBody);
+                c.startActivity(Intent.createChooser(emailIntent, whichEmailToUse));
+
+                break;
+            case R.id.btn_no:
+                //Toast.makeText(c.getApplicationContext(), c.getResources().getString(R.string.notsupport_text), Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+        dismiss();
+    }
+}
